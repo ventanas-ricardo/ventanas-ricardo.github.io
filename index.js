9fb3abc58b8186df270754acd45492baaa36bf3b
ñoquis <leonzg4231@gmail.com>
2025-05-19 10:27:55 -0300

    lots o crap

---
+++ index.js
@@ -0,0 +1,12 @@
+function buy_click(index) {
+    window.location.href = "item" + String(index) + ".html";
+}
+
+window.buy_click = buy_click;
+
+function get_back() {
+    window.location.href = "index.html";
+}
+
+window.buy_click = buy_click;
+window.get_back=   get_back;
