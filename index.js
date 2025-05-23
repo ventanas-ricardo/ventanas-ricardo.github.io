7bacc3203915a97c70091e72e3f4a811de3e92d7
ñoquis <leonzg4231@gmail.com>
2025-05-23 12:55:30 -0300

    init sound, and more

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+window.onload = function() 
+{
+let audio=new Audio("assets/sound/bienvenidoaricardo.wav");
+audio.play();
+}
+
+
 function buy_click(index) {
-    window.location.href = "item" + String(index) + ".html";
+    window.location.href = "sub/item" + String(index) + ".html";
 }
 
 window.buy_click = buy_click;
-
-function get_back() {
-    window.location.href = "index.html";
-}
-
-window.buy_click = buy_click;
-window.get_back=   get_back;
---
+++ sub/item.js
@@ -0,0 +1,6 @@
+function get_back() 
+{
+    window.location.href = "../index.html";
+}
+
+window.get_back=get_back;
